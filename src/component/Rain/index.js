4bf3003a3b1703cfb84f8d152fe88d91edbe17a7
Skyline-9 <luo.richard@gmail.com>
2022-03-12 13:59:16 -0500

    :sparkles: Remade homepage look

---
+++ src/component/Rain/index.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
+class Rain extends Component {
+    startRain() {
+        const rainSection = document.getElementById('Rain');
+
+        for (let i = 1; i < this.props.numDrops; i++) {
+            const dropLeft = this.randRange(0, 1600);
+            const dropTop = this.randRange(-1000, 1400);
+
+            const drop = document.createElement('div');
+
+            drop.setAttribute('class', 'drop');
+            drop.setAttribute('id', `drop${i}`);
+
+            rainSection.appendChild(drop);
+
+            drop.style.left = `${dropLeft}px`;
+            drop.style.top = `${dropTop}px`;
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.numDrops !== prevProps.numDrops) {
+            this.stopRain();
+            this.startRain()
+        }
+    }
+
+    stopRain() {
+        const rainSection = document.getElementById('Rain');
+
+        while (rainSection.hasChildNodes()) {
+            rainSection.removeChild(rainSection.lastChild);
+        }
+    }
+
+    randRange(minNum, maxNum) {
+        return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+    }
+
+    componentDidMount() {
+        this.startRain();
+    }
+
+    render() {
+        return (<div id="Rain"/>);
+    }
+}
+
+Rain.propTypes = {
+    numDrops: PropTypes.number.isRequired
+};
+
+export default Rain;

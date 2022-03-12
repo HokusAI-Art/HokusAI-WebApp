f051e6ec47767a93a1422abbd3186c975e155973
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-03-12 12:08:29 -0500

    added sample 3d view

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import firebase from 'firebase/compat/app';
 import VR from "./component/VR";
 
 const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_API_KEY);
+
 export const app = firebase.initializeApp(firebaseConfig);
 
 // constants to keep track of url routes
@@ -18,11 +19,13 @@ export const k_landing_route = "/";
 const App = () => {
   return (
       <Router>
+        {/* A <Router> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
         <Routes>
-          {/*<Route path={k_home_route} element={<Home/>}/>*/}
-          {/*<Route path={k_immersive_view_route} element={<VR/>}/>*/}
-          <Route path={k_landing_route} element={<Landing/>}/>
-          <Route path="*" element={<Navigate to={k_landing_route}/>}/>
+            <Route path={k_home_route} element={<Home/>}/>
+            <Route path={k_immersive_view_route} element={<VR/>}/>
+            <Route path={k_landing_route} element={<Landing/>}/>
+            <Route path="*" element={<Navigate to={k_landing_route}/>}/>
         </Routes>
       </Router>
   );
--- src/component/VR/index.js
+++ src/component/VR/index.js
@@ -80,10 +80,169 @@ const VR = () => {
             />
 
             { user &&
-                <div>hello vr view</div>
+                <VRCanvas>
+                    <Physics
+                        gravity={[0, 0, 0]}
+                        iterations={20}
+                        defaultContactMaterial={{
+                            friction: 0.09
+                        }}>
+                        <Scene artworks={artworks}/>
+                    </Physics>
+                </VRCanvas>
             }
         </div>
     );
+}
+
+const Scene = ({artworks}) => {
+    const { player } = useXR()
+    const leftController = useController('left');
+    const rightController = useController('right');
+    const [debugText, setDebugText] = useState("");
+    const cameraRef = useRef();
+
+    let lastUpdatedTime = 0;
+    const globalYOffset = -1;
+
+    let [initPlayer, setInitPlayer] = useState(false);
+
+    const [floorRef] = usePlane(() => ({
+        args: [10, 10],
+        rotation: [-Math.PI / 2, 0, 0],
+        position: [0, -0.1, 0],
+        type: 'Static'
+    }))
+
+    useXRFrame((time, xrFrame) => {
+        // do something on each frame of an active XR session
+        console.log(time);
+
+        if (!initPlayer) {
+            player.position.x = -2;
+            player.position.z = 9;
+            player.position.y = globalYOffset;
+
+            setInitPlayer(true);
+            initPlayer = true;
+        }
+
+        if (lastUpdatedTime === 0) {
+            lastUpdatedTime = time;
+        }
+
+        const dt = time - lastUpdatedTime;
+
+        let leftGamepad = leftController.inputSource.gamepad;
+        let rightGamepad = rightController.inputSource.gamepad;
+
+        // right gamepad
+        if (rightGamepad) {
+            if (rightGamepad.buttons[0].pressed) {
+                // index finger big button
+                // setDebugText("index finger button pressed");
+            }
+
+            if (rightGamepad.buttons[1].pressed) {
+                // middle finger button big button
+                // setDebugText("middle finger button pressed");
+            }
+
+            const joystickXAxis = rightGamepad.axes[2];
+            const joystickYAxis = rightGamepad.axes[3];
+        }
+
+        // left gamepad
+        if (leftGamepad) {
+            if (leftGamepad.buttons[0].pressed) {
+                // index finger big button
+                // setDebugText("index finger button pressed");
+            }
+
+            if (leftGamepad.buttons[1].pressed) {
+                // middle finger button big button
+                // setDebugText("middle finger button pressed");
+            }
+
+            const joystickXAxis = leftGamepad.axes[2];
+            const joystickYAxis = leftGamepad.axes[3];
+
+            const speedScale = 2;
+            const speed = 0.0005 * speedScale;
+            const velX = joystickXAxis * speed * dt;
+            const velZ = joystickYAxis * speed * dt;
+
+            player.position.x += velX;
+            player.position.z += velZ;
+
+            // setDebugText("Axis X: " + rightControllerJoystickXAxis);
+            // setDebugText("dt: " + dt);
+        }
+
+        lastUpdatedTime = time;
+    })
+
+    const gltf = useLoader(GLTFLoader, 'scene.gltf');
+
+    return (
+        <>
+            <Sky />
+            {/*<Plane ref={floorRef} args={[50, 50]} receiveShadow>*/}
+            {/*    <meshStandardMaterial attach="material" color="#8ea1bf" />*/}
+            {/*</Plane>*/}
+            <Suspense fallback={null}>
+                <primitive object={gltf.scene} position={[0, -0.3 + globalYOffset, 7]} rotation={[0, Math.PI/2, 0]} scale={[0.7, 0.7, 0.7]}/>
+            </Suspense>
+            <DefaultXRControllers />
+            {artworks.map((artwork, i) => {
+                const limit = 7;
+                const initX = -5;
+                const initY = 1.4 + globalYOffset;
+                const initZ = 3.66;
+                const imageScale = 0.8;
+                const imageWidth = 1.6 * imageScale;
+                const imageHeight = 1 * imageScale;
+                const imageDistance = 0.5 * imageScale;
+
+                if (i < limit) {
+                    return (
+                        <Artwork key={artwork.id} artwork={artwork} width={imageWidth} height={imageHeight}
+                                 position={[initX + i * (imageWidth + imageDistance), initY, initZ]}/>
+                    );
+                }
+            })}
+            <Text color={'black'} position={[0.0, 1.1, -0.5]}>{debugText}</Text>
+            {/*<PerspectiveCamera position={[5, 5, 5]} />*/}
+            {/*<OrbitControls ref={cameraRef}/>*/}
+            <PointerLockControls ref={cameraRef} />
+            <ambientLight intensity={0.5} />
+            <spotLight position={[1, 8, 1]} angle={0.3} penumbra={1} intensity={1} castShadow />
+        </>
+    )
+}
+
+const Artwork = ({artwork, width, height, position}) => {
+    const texture = useLoader(THREE.TextureLoader, artwork.imageUrl);
+
+    return (
+        <>
+            <mesh position={position}>
+                <planeBufferGeometry attach="geometry" args={[width, height]} />
+                <meshBasicMaterial attach="material" map={texture} />
+            </mesh>
+
+            {
+                artwork.loading ? (
+                    <Text color={'black'} position={[position[0], position[1] - height / 2 - 0.1, position[2]]}>{'Processing...'}</Text>
+                ) : (
+                    <>
+                        <Text color={'black'} position={[position[0], position[1] - height / 2 - 0.1, position[2]]}>{artwork.text}</Text>
+                        <Text color={'black'} position={[position[0], position[1] - height / 2 - 0.3, position[2]]}>created by {artwork.user.displayName}</Text>
+                    </>
+                )
+            }
+        </>
+    )
 }
 
 export default VR;

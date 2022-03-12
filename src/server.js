5df1d7f1e18b8d70d991a333d9fc669274d308f6
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-03-12 11:34:50 -0500

    added env local to gitignore

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+const app = express(); // create express app
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => {
+    console.log(`Web app server is listening at http://localhost:${PORT}`);
+});

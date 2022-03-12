22576fbf6988108d3cfe3d38666dbea15c9b3903
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-03-12 11:35:51 -0500

    test

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 
-const app = express(); // create express app
+const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
